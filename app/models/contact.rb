722de546e38eec5c9f82e9eac21a110843057b30
Alfian Losari <alfian@losari.org>
2015-01-14 16:35:15 +0800

    Save data to spreadsheet

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
+require 'google_drive_v0'
+
 class Contact
 
   include ActiveModel::Model
@@ -9,5 +11,24 @@ class Contact
   validates :name, :email, :content, presence: true
   validates :email, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
   validates :content, length: { maximum: 500 }
+
+  def update_spreadsheet
+
+    connection = GoogleDriveV0.login(Rails.application.secrets.email_provider_username, Rails.application.secrets.email_provider_password)
+    ss = connection.spreadsheet_by_title("Learn-Rails-Example")
+    if ss.nil?
+      ss = connection.create_spreadsheet("Learn-Rails-Example")
+    end
+
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    ws[last_row, 1] = Time.new
+    ws[last_row, 2] = self.name
+    ws[last_row, 3] = self.email
+    ws[last_row, 4] = self.content
+    ws.save
+    
+  end
+
 
 end
